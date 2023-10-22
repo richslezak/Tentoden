eb55fc7431dc39db0bc9f4efeaa2a6de6ff22e12
richslezak <richslezak@gmail.com>
2023-10-22 16:04:04 -0400

    replacing deployment with work and pictures

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -1,3 +1,4 @@
-import 'bootstrap/dist/js/bootstrap.min.js';
-// import 'bootstrap/js/dist/offcanvas.js';
+// import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/js/dist/dropdown.js';
 // import 'bootstrap/js/dist/collapse.js';
+import 'bootstrap/js/dist/offcanvas.js';
