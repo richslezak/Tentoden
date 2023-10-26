dfaf632b6a45089026950fb87b9f112eb024f70f
richslezak <richslezak@gmail.com>
2023-10-26 11:24:10 -0400

    update to about page

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -1,5 +1,5 @@
 // import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap/js/dist/dropdown.js';
 // import 'bootstrap/js/dist/collapse.js';
+import 'bootstrap/js/dist/dropdown.js';
 import 'bootstrap/js/dist/offcanvas.js';
 import 'bootstrap/js/dist/carousel.js';
