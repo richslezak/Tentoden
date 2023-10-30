c64891b7e36fbea852eb2addff7dd073b2269f58
richslezak <richslezak@gmail.com>
2023-10-30 14:54:57 -0400

    site description and key words

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@
 
 export const SITE_TITLE = 'Tentoden';
 export const SITE_DESCRIPTION =
-  'Tendoden Your Brand Identity and Web Design Partner. We are a full-service design agency that specializes in branding, web design, and web development.';
+  'Tentoden is a full-service company that specializes in branding, graphic design, UI design, UX design, web design, and web development.';
 export const SITE_KEYWORDS =
   'Tendoden, Graphic Design, Web Design, Web Development, Customer Service, Marketing, Social Media, Social Media Marketing, Social Media Management, Social Media Advertising, Social Media Ads, Social';
