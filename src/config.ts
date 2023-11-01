5ebff84b490b4cc80b19081f7abf69b2e6afe9b0
richslezak <richslezak@gmail.com>
2023-11-01 16:55:19 -0400

    created rich slezak page and update meta tags

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,8 @@
 
 export const SITE_TITLE = 'Tentoden';
 export const SITE_DESCRIPTION =
-  'Tentoden is a full-service company that specializes in branding, graphic design, UI design, UX design, web design, and web development.';
+  'Tentoden: A portfolio and design & development services by Richard Slezak, available for hire remotely and in Boston, MA.';
 export const SITE_KEYWORDS =
-  'Tendoden, Graphic Design, Web Design, Web Development, Customer Service, Marketing, Social Media, Social Media Marketing, Social Media Management, Social Media Advertising, Social Media Ads, Social';
+  'Tendoden, Graphic Design, Web Design, UX Design, UI Development, Web Development, Branding, Digital Success, Customer Service, Marketing, Social Media, Social Media Marketing, Social Media Management, Social Media Advertising, Social Media Ads, Social, Boston, Massachusetts, For Hire, Contract Work, Remote Work, Team Work, Richard Slezak';
+export const SITE_AUTHOR = 'Richard Slezak';
+export const SITE_URL = 'https://tentoden.com';
